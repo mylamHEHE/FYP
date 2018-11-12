b70f644d462326c04e5f46f48a7653bb148e31a4
mylamHEHE <mymichael428@hotmail.com>
2018-11-12 15:02:43 +0800

    scan not complete

--- app/src/main/java/com/example/user/smartfitnesstrainer/Main/Bluetooth_reserve/CameraFragment.java
+++ app/src/main/java/com/example/user/smartfitnesstrainer/Main/Bluetooth_reserve/CameraFragment.java
@@ -1,15 +1,28 @@
 package com.example.user.smartfitnesstrainer.Main.Bluetooth_reserve;
 
 import android.app.Fragment;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
+import com.example.user.smartfitnesstrainer.Main.HomeActivity;
+import com.example.user.smartfitnesstrainer.Main.ScanActivity;
 import com.example.user.smartfitnesstrainer.R;
 import com.gigamole.library.PulseView;
+
+import java.util.Set;
 
 import pl.bclogic.pulsator4droid.library.PulsatorLayout;
 
@@ -17,6 +30,8 @@ import pl.bclogic.pulsator4droid.library.PulsatorLayout;
 public class CameraFragment extends android.support.v4.app.Fragment {
     private  static final String TAG = "CameraFragment";
     public PulsatorLayout pulsator;
+    ImageButton opentooth;
+    ImageButton openscan;
     private boolean runningThread = false;
     @Nullable
     @Override
@@ -26,9 +41,34 @@ public class CameraFragment extends android.support.v4.app.Fragment {
 
         Log.d("yes","yes");
 
+        opentooth=(ImageButton)view.findViewById(R.id.opentooth);
+        opentooth.bringToFront();
+        opentooth.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                final Intent intent = new Intent(Intent.ACTION_MAIN, null);
+                intent.addCategory(Intent.CATEGORY_LAUNCHER);
+                ComponentName cn = new ComponentName("com.android.settings",
+                        "com.android.settings.bluetooth.BluetoothSettings");
+                intent.setComponent(cn);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+        openscan=(ImageButton)view.findViewById(R.id.openscan);
+        openscan.bringToFront();
+        openscan.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent homeIntent = new Intent(getContext() ,ScanActivity.class);
+                startActivity(homeIntent);
+            }
+        });
+
         pulsator.start();
         return view;
     }
+
 
     @Override
     public void onPause() {
--- app/src/main/java/com/example/user/smartfitnesstrainer/Main/CameraFragment.java
+++ app/src/main/java/com/example/user/smartfitnesstrainer/Main/CameraFragment.java
@@ -1,21 +1,48 @@
 package com.example.user.smartfitnesstrainer.Main;
 
 import android.app.Fragment;
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.user.smartfitnesstrainer.R;
 
 public class CameraFragment extends android.support.v4.app.Fragment {
     private  static final String TAG = "CameraFragment";
-
+    ImageButton opentooth;
+    ImageButton openscan;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_camera,container,false);
+        opentooth=(ImageButton)view.findViewById(R.id.opentooth);
+        opentooth.bringToFront();
+        opentooth.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                final Intent intent = new Intent(Intent.ACTION_MAIN, null);
+                intent.addCategory(Intent.CATEGORY_LAUNCHER);
+                ComponentName cn = new ComponentName("com.android.settings",
+                        "com.android.settings.bluetooth.BluetoothSettings");
+                intent.setComponent(cn);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity( intent);
+            }
+        });
+        openscan=(ImageButton)view.findViewById(R.id.openscan);
+        openscan.bringToFront();
+        openscan.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(getActivity(),"BMI is clicked.",Toast.LENGTH_LONG).show();
+            }
+        });
         return view;
     }
 }
