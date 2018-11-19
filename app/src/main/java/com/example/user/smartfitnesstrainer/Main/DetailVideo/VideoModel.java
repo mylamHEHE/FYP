da251e19386aa872dd3f5de0f426eccce6e69704
tomilia <koekax@gmail.com>
2018-11-20 02:31:34 +0800

    video entering

---
+++ app/src/main/java/com/example/user/smartfitnesstrainer/Main/DetailVideo/VideoModel.java
@@ -0,0 +1,12 @@
+package com.example.user.smartfitnesstrainer.Main.DetailVideo;
+
+import android.net.Uri;
+
+public class VideoModel {
+    int videoUrl;
+
+    public VideoModel(int videoUrl) {
+        this.videoUrl = videoUrl;
+    }
+
+}
