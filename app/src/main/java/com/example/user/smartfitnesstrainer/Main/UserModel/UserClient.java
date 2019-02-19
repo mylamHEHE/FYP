9675d78d60c93f71ba1076c11030796649be191e
tomilia <koekax@gmail.com>
2019-02-20 00:44:23 +0800

    Online modeling

--- app/src/main/java/com/example/user/smartfitnesstrainer/Main/UserModel/UserClient.java
+++ app/src/main/java/com/example/user/smartfitnesstrainer/Main/UserModel/UserClient.java
@@ -1,5 +1,10 @@
 package com.example.user.smartfitnesstrainer.Main.UserModel;
 
+import com.example.user.smartfitnesstrainer.Main.Profile.UserProfile;
+import com.example.user.smartfitnesstrainer.Main.exercise_selection_page.Playlist;
+
+import java.util.List;
+
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -11,5 +16,7 @@ public interface UserClient {
     @POST("login")
     Call<User> login(@Body Login login);
     @GET("secret")
-    Call<ResponseBody> getProfile(@Header("Content-Type") String content_type,@Header("Authorization") String authToken);
+    Call<UserProfile> getProfile(@Header("Content-Type") String content_type, @Header("Authorization") String authToken);
+    @GET("playlist")
+    Call<List<Playlist>> getPlaylist(@Header("Content-Type") String content_type, @Header("Authorization") String authToken);
 }
--- app/src/main/java/com/example/user/smartfitnesstrainer/Main/UserModel/UserProfile.java
+++
@@ -1,5 +0,0 @@
-package com.example.user.smartfitnesstrainer.Main.UserModel;
-
-public class UserProfile {
-
-}
